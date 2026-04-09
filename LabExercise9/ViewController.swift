687cbe3f6200df4b3e79dc070dd69c5d0667d304
Woohyuk Song <harrywsong@naver.com>
2026-04-08 21:12:48 -0400

    lab exercise 9

--- LabExercise9/ViewController.swift
+++ LabExercise9/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taskTextField: UITextField!
+
+    @IBAction func addTaskTapped(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
