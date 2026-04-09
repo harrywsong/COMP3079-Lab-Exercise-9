5bdcb49b03c90e657ccff310814dcdebfb8a77f6
Woohyuk Song <harrywsong@naver.com>
2026-04-08 21:25:02 -0400

    lab exercise 9

--- LabExercise9/TaskListViewControllerTableViewController.swift
+++ LabExercise9/TaskListViewControllerTableViewController.swift
@@ -6,84 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskListViewControllerTableViewController: UITableViewController {
 
+    var tasks: [Task] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TaskCell")
+        fetchTasks()
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchTasks()
+        tableView.reloadData()
     }
+
+    func fetchTasks() {
+        tasks = (try? context.fetch(Task.fetchRequest())) ?? []
+    }
+
+    // MARK: - Table view
+    override func numberOfSections(in tableView: UITableView) -> Int { return 1 }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tasks.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row].title
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Swipe Actions
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { _, _, done in
+            self.context.delete(self.tasks[indexPath.row])
+            try? self.context.save()
+            self.tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            done(true)
+        }
+
+        let edit = UIContextualAction(style: .normal, title: "Edit") { _, _, done in
+            let alert = UIAlertController(title: "Edit Task", message: nil, preferredStyle: .alert)
+            alert.addTextField { $0.text = self.tasks[indexPath.row].title }
+            alert.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+                self.tasks[indexPath.row].title = alert.textFields?.first?.text
+                try? self.context.save()
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            })
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            self.present(alert, animated: true)
+            done(true)
+        }
+        edit.backgroundColor = .systemBlue
+
+        return UISwipeActionsConfiguration(actions: [delete, edit])
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- LabExercise9/ViewController.swift
+++ LabExercise9/ViewController.swift
@@ -6,20 +6,26 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var taskTextField: UITextField!
-
+    
     @IBAction func addTaskTapped(_ sender: UIButton) {
+        guard let title = taskTextField.text, !title.isEmpty else { return }
         
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let task = Task(context: context)
+        task.title = title
+        task.createdAt = Date()
+        try? context.save()
+        
+        taskTextField.text = ""
+        performSegue(withIdentifier: "showTaskList", sender: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
